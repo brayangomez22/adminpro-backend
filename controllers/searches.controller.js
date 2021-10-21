67eb5a7d409f465143017ee6094cd235fd59bb54
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-10-20 20:34:40 -0500

    General search in all collections simultaneously

--- controllers/searches.controller.js
+++ controllers/searches.controller.js
@@ -1,11 +1,19 @@
 const { request, response } = require('express');
+const User = require('../models/user.model');
+const Hospital = require('../models/hospital.model');
+const Doctor = require('../models/doctor.model');
 
 const getAll = async (req = request, res = response) => {
 	const search = req.params.search;
+	const regex = new RegExp(search, 'i');
+
+	const [users, hospitals, doctors] = await Promise.all([User.find({ name: regex }), Hospital.find({ name: regex }), Doctor.find({ name: regex })]);
 
 	res.json({
 		ok: true,
-		search,
+		users,
+		hospitals,
+		doctors,
 	});
 };
 
