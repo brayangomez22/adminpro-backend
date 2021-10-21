2b559392158d610069dfa35789db52ca6217b1a5
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-10-21 10:13:43 -0500

    The search service by specific collection was implemented

--- controllers/searches.controller.js
+++ controllers/searches.controller.js
@@ -17,6 +17,40 @@ const getAll = async (req = request, res = response) => {
 	});
 };
 
+const getDocumentsCollection = async (req = request, res = response) => {
+	const table = req.params.table;
+	const search = req.params.search;
+	const regex = new RegExp(search, 'i');
+
+	let data = [];
+
+	switch (table) {
+		case 'doctors':
+			data = await Doctor.find({ name: regex }).populate('user', 'name img').populate('hospital', 'name img');
+			break;
+
+		case 'hospitals':
+			data = await Hospital.find({ name: regex }).populate('user', 'name img');
+			break;
+
+		case 'users':
+			data = await User.find({ name: regex });
+			break;
+
+		default:
+			return res.status(400).json({
+				ok: false,
+				msg: 'The table has to be doctors / hospitals / users',
+			});
+	}
+
+	res.status(200).json({
+		ok: true,
+		results: data,
+	});
+};
+
 module.exports = {
 	getAll,
+	getDocumentsCollection,
 };
--- routes/searches.routes.js
+++ routes/searches.routes.js
@@ -2,11 +2,12 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validateFields } = require('../middlewares/validate-fields');
-const { getAll } = require('../controllers/searches.controller');
+const { getAll, getDocumentsCollection } = require('../controllers/searches.controller');
 const { validateJWT } = require('../middlewares/validate-jwt');
 
 const router = Router();
 
 router.get('/:search', validateJWT, getAll);
+router.get('/collection/:table/:search', validateJWT, getDocumentsCollection);
 
 module.exports = router;
